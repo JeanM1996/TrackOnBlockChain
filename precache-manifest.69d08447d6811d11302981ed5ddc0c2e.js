a054fae5190e941d6e09a6dafab9846d4e01e4bb
Jean-Paul Mosquera Arevalo <jpmosquera1@utpl.edu.ec>
2020-08-02 08:59:06 -0500

    Updates

---
+++ precache-manifest.69d08447d6811d11302981ed5ddc0c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84b147a8a25b598958fe",
+    "url": "/coffeetrack/static/js/runtime~main.e8ea2a5e.js"
+  },
+  {
+    "revision": "763cdbadaa89c36498a7",
+    "url": "/coffeetrack/static/js/main.7486a6e7.chunk.js"
+  },
+  {
+    "revision": "506196cc7b799082f14c",
+    "url": "/coffeetrack/static/js/2.0740ac68.chunk.js"
+  },
+  {
+    "revision": "763cdbadaa89c36498a7",
+    "url": "/coffeetrack/static/css/main.e20d7b5c.chunk.css"
+  },
+  {
+    "revision": "506196cc7b799082f14c",
+    "url": "/coffeetrack/static/css/2.08228282.chunk.css"
+  },
+  {
+    "revision": "3a34b57186094428141f456622b827f3",
+    "url": "/coffeetrack/index.html"
+  }
+];
